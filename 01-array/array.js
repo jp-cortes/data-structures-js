7a7d25b3a81d755abf336d8edf9604b7b9ef2f2e
Juan Cortes <alias99002@outlook.com>
2023-02-20 10:37:07 -0500

    hashTable

--- 01-array/array.js
+++ 01-array/array.js
@@ -2,21 +2,21 @@
 
 class MyArray {
     constructor() {
-        this.lenght = 0;
+        this.length = 0;
         this.data = {};
     }
     get(index) {
         return this.data[index];
     }
     push(item) {
-        this.data[this.lenght] = item;
-        this.lenght++;
+        this.data[this.length] = item;
+        this.length++;
         return this.data;
     }
     pop() {
-       const lastItem = this.data[this.lenght - 1];
-       delete this.data[this.lenght - 1];
-       this.lenght--;
+       const lastItem = this.data[this.length - 1];
+       delete this.data[this.length - 1];
+       this.length--;
 
         return lastItem;
     }
@@ -26,11 +26,35 @@ class MyArray {
         return item;
     }
     shiftIndex(index) {
-        for (let i = index; i < this.lenght - 1; i++) {
+        for (let i = index; i < this.length - 1; i++) {
             this.data[i] = this.data[i + 1];
         }
-        delete this.data[this.lenght - 1];
-        this.lenght--;
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+    shift() {
+        const firstItem = this.data[0];
+        delete this.data[0];
+        this.length--;
+ 
+         return firstItem;
+    }
+    unShift(item) {
+        if(!item) {
+            return this.length;
+        }
+        if(item === 0) {
+            this.data[0] = item;
+            this.length++;
+            return this.data;
+        }
+        for (let i = this.length - 1; i >= 0; i--) {
+            this.data[i + 1] = this.data[i];
+        }
+        this.data[0] = item;
+        this.length++;
+       
+        return this.length;
     }
 }
 
